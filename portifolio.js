e660f76b426bcc0851f1e83fca2a646c508e243b
Wanderson Santos Lemos <wanderson.2002.2017@edu.uniube.br>
2025-09-29 18:25:37 -0300

    Portfólio inicial

---
+++ portifolio.js
@@ -0,0 +1,19 @@
+
+ScrollReveal().reveal('.txt_sobre_mim,.sobre_mim,.sobre_mim_c,.tracinho_texto,.texto,.tracinho_texto2,.iii,.card_habilidade,.tracinho_projetos,.projetos_txt,.div_saudacao_meu_nome,.saudaca,.Meu_nome,.descricao,.icone_lin,.icone_telefone,.tracinho,.tracinho_2', {
+  delay: 200,
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  reset:true
+  
+});
+
+
+ScrollReveal().reveal('.div_saudacao_meu_nome,.saudaca,.Meu_nome,.descricao,.icone_lin,.icone_telefone,.tracinho,.tracinho_2,.icones', {
+  delay: 200,
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  reset:true
+  
+});
